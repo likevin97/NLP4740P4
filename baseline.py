395073aa89d1da2379413ee21508d1163e01d6a9
rebeccaj1211 <rebeccaj1211@gmail.com>
2018-11-27 20:29:01 -0500

    baseline code

---
+++ baseline.py
@@ -0,0 +1,63 @@
+import json
+import nltk
+from collections import Counter
+
+# Simplified question
+def getSimpleQuestion(question):
+    text = nltk.word_tokenize(question)
+    words = nltk.pos_tag(text)
+    less_words = [wt for (wt, tag) in words if tag not in ["CC","DT","EX","IN","LS","POS","TO",".","\\",",",":","(",")"]]
+    return less_words
+
+def countWordsInParagraph(context):
+    wordList = nltk.word_tokenize(context)
+    counts = Counter(wordList)
+    return counts
+
+def main():
+	file = open("training.json")
+	j = json.load(file)
+
+	predictions = {} #id:value
+
+	data_length = len(j[u'data']) #442
+	for data in range(data_length):
+		paragraph_length = len(j[u'data'][data][u'paragraphs']) #66
+
+		for paragraph in range(paragraph_length):
+			context = j[u'data'][data][u'paragraphs'][paragraph][u'context']
+			question_length = len(j[u'data'][data][u'paragraphs'][paragraph][u'qas'])
+
+			dic = countWordsInParagraph(context)
+
+			for q in range(question_length):
+				question = j[u'data'][data][u'paragraphs'][paragraph][u'qas'][q]["question"]
+				ids = j[u'data'][data][u'paragraphs'][paragraph][u'qas'][q]["id"]
+
+				#only training has is_impossible
+				#is_impossible = j[u'data'][data][u'paragraphs'][paragraph][u'qas'][q]["is_impossible"]
+
+				#deletes useless information from question
+				new_question = getSimpleQuestion(question)
+				total = 0
+				for word in new_question:
+					if word in dic:
+						total+= dic[word]
+
+				if total >= 1:
+					predictions[ids] = 1
+				else:
+					predictions[ids] = 0
+
+
+				#print (question)
+				#print (new_question)
+				#print (is_impossible)
+				#print (total)
+				#break
+			print (predictions)
+			#break
+		#break
+
+
+main()
