febc622ec743dcd57fef2d5ae91e82326a4bc865
likevin97 <kl553@cornell.edu>
2018-12-01 18:59:56 -0500

    Implemented similarity in baseline3

--- baseline3.py
+++ baseline3.py
@@ -1,6 +1,9 @@
 import json
 import nltk
 from collections import Counter
+import numpy as np
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics import jaccard_similarity_score
 
 # Simplified question
 def getSimpleQuestion(question):
@@ -14,101 +17,90 @@ def countWordsInParagraph(context):
     counts = Counter(wordList)
     return counts
 
-def uniqueWordsInDataset(prev_set, new_words):
-    words = set(nltk.word_tokenize(new_words))
-    new_set = prev_set.union(words)
-    return new_set
-
-def createEmptyVector(filename):
+def createCorpus(filename):
     file = open(filename)
     j = json.load(file)
-    unique_words = set()
+
+    corpus = []
 
     data_length = len(j[u'data']) #442
     for data in range(data_length):
         paragraph_length = len(j[u'data'][data][u'paragraphs']) #66
 
         for paragraph in range(paragraph_length):
-            context = j[u'data'][data][u'paragraphs'][paragraph][u'context']
+            context = j[u'data'][data][u'paragraphs'][paragraph][u'context'].lower()
+
+            corpus.append(context)
+
             question_length = len(j[u'data'][data][u'paragraphs'][paragraph][u'qas'])
 
-            unique_words = uniqueWordsInDataset(unique_words, context)
-
             for q in range(question_length):
-                question = j[u'data'][data][u'paragraphs'][paragraph][u'qas'][q]["question"]
-                
-                unique_words = uniqueWordsInDataset(unique_words, question)
+                question = j[u'data'][data][u'paragraphs'][paragraph][u'qas'][q]["question"].lower()
 
-    return unique_words
+                corpus.append(question)
+    file.close()
+    return corpus
+
 
 def similarity(context_vector, question_vector):
     #FIND THE SIMILARITY SOMEHOW
     #returns the similarity
     pass
+
+
 def main():
+    corpus = createCorpus("training_sample.json")
+
     file = open("training_sample.json")
     j = json.load(file)
 
     predictions = {} #id:value
-    unique_words = createEmptyVector("training_sample.json")
-    vec_length = len(unique_words) #get length of set
-    index = {}
-    count = 0
-    for word in unique_words:
-            index[word] = count
-            count += 1
 
-    emptyVec = [0 for i in range(vec_length)]
     similarity = {}
 
+    vectorizer = CountVectorizer()
+    vectors = vectorizer.fit_transform(corpus)
+    vectorsArrayForm = vectors.toarray()
+
+    counter = 0
 
     data_length = len(j[u'data']) #442
     for data in range(data_length):
+
         paragraph_length = len(j[u'data'][data][u'paragraphs']) #66
-
         for paragraph in range(paragraph_length):
-            context = j[u'data'][data][u'paragraphs'][paragraph][u'context'].split()
+
+            context_vector = vectorsArrayForm[counter]
+
+            counter += 1
+
             question_length = len(j[u'data'][data][u'paragraphs'][paragraph][u'qas'])
-
-            vector_context = emptyVec
-            for word in context:
-                vector_context[index[word]] += 1
-
             for q in range(question_length):
-                question = j[u'data'][data][u'paragraphs'][paragraph][u'qas'][q]["question"]
-                ids = j[u'data'][data][u'paragraphs'][paragraph][u'qas'][q]["id"]
+                question_id = j[u'data'][data][u'paragraphs'][paragraph][u'qas'][q]["id"]
 
-                #only training has is_impossible
-                #is_impossible = j[u'data'][data][u'paragraphs'][paragraph][u'qas'][q]["is_impossible"]
-                vector_question = emptyVec
-                for word in question:
-                    vector_question[index[word]] += 1
+                question_vector = vectorsArrayForm[counter]
 
+                similarity[question_id] = jaccard_similarity_score(context_vector, question_vector)
+
+                counter += 1
 
 
-                print (vector_question)
-                print ("-----")
-                print (vector_context)
-                print ("-----")
-                print (index)
-                break
-            break
-        break
-                #s = similarity(vector_context, vector_quesetion)
+    print(similarity)
+    #s = similarity(vector_context, vector_quesetion)
 
 
-                # #deletes useless information from question
-                # new_question = getSimpleQuestion(question)
-                # total = 0
-                # for word in new_question:
-                # 	if word in dic:
-                # 		total+= dic[word]
+    # #deletes useless information from question
+    # new_question = getSimpleQuestion(question)
+    # total = 0
+    # for word in new_question:
+    # 	if word in dic:
+    # 		total+= dic[word]
 
-                # if total >= 1:
-                # 	predictions[ids] = 1
-                # else:
-                # 	predictions[ids] = 0
-    with open('output3.json', 'w') as outfile:  
-        json.dump(predictions, outfile)
+    # if total >= 1:
+    # 	predictions[ids] = 1
+    # else:
+    # 	predictions[ids] = 0
+    # with open('output3.json', 'w') as outfile:  
+    #     json.dump(predictions, outfile)
 
 main()
