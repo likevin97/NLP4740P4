195b3d909308f6ff31f4920ef445c1871f20badf
rebeccaj1211 <rebeccaj1211@gmail.com>
2018-12-05 19:33:36 -0500

    baseline2

--- baseline2.py
+++ baseline2.py
@@ -1,12 +1,17 @@
 import json
 import nltk
 from collections import Counter
+from nltk.corpus import stopwords
 
 # Simplified question
 def getSimpleQuestion(question):
-	text = nltk.word_tokenize(question)
-	words = nltk.pos_tag(text)
-	less_words = [wt for (wt, tag) in words if tag not in ["CC","DT","EX","IN","LS","POS","TO",".","\\",",",":","(",")"]]
+	text = nltk.word_tokenize(question, stop_words = None)
+	less_words = []
+	if stop_words == None:
+		words = nltk.pos_tag(text)
+		less_words = [wt for (wt, tag) in words if tag not in ["CC","DT","EX","IN","LS","POS","TO",".","\\",",",":","(",")"]]
+	else:
+		less_words = [w for w in text if w not in stop_words]
 	return less_words
 
 def countWordsInParagraph(context):
@@ -15,6 +20,9 @@ def countWordsInParagraph(context):
 	return counts
 
 def main():
+	stop_words = set(stopwords.words('english'))
+	stop_words.union(set(string.punctuation))
+
 	file = open("training.json")
 	j = json.load(file)
 
@@ -38,13 +46,13 @@ def main():
 				#is_impossible = j[u'data'][data][u'paragraphs'][paragraph][u'qas'][q]["is_impossible"]
 
 				#deletes useless information from question
-				new_question = getSimpleQuestion(question)
+				new_question = getSimpleQuestion(question, stop_words)
 				total = 0
 				for word in new_question:
 					if word in dic:
 						total+= dic[word]
 
-				if total >= 9:
+				if total >= 7:
 					predictions[ids] = 1
 				else:
 					predictions[ids] = 0
